b9e8d19c0837bd1f0b7940f49986c8cbdfcd529f
Signior-X <sethpriyam1@gmail.com>
2022-09-29 12:02:15 +0530

    change cookie parser

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from "@nestjs/platform-express";
 import { join } from "path";
-import * as cookieParser from 'cookie-parser';
+import cookieParser from 'cookie-parser';
 
 import { ConfigService } from '@nestjs/config';
 import { ValidationPipe } from '@nestjs/common';
