6411cdef9dece89b3a1911081ea517e11cc7e9fd
Signior-X <sethpriyam1@gmail.com>
2022-09-29 13:30:25 +0530

    Fix cookie parser

--- dist/main.js
+++ dist/main.js
@@ -3,7 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const core_1 = require("@nestjs/core");
 const app_module_1 = require("./app.module");
 const path_1 = require("path");
-const cookieParser = require("cookie-parser");
+const cookie_parser_1 = require("cookie-parser");
 const hbs = require("hbs");
 const config_1 = require("@nestjs/config");
 const common_1 = require("@nestjs/common");
@@ -11,7 +11,7 @@ async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
     app.useGlobalPipes(new common_1.ValidationPipe());
     const configService = app.get(config_1.ConfigService);
-    app.use(cookieParser());
+    app.use((0, cookie_parser_1.default)());
     app.useStaticAssets((0, path_1.join)(__dirname, '..', 'public'));
     app.setBaseViewsDir((0, path_1.join)(__dirname, '..', 'views'));
     app.setViewEngine('hbs');
--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from "@nestjs/platform-express";
 import { join } from "path";
-import * as cookieParser from 'cookie-parser';
+import cookieParser from 'cookie-parser';
 import * as hbs from 'hbs';
 
 import { ConfigService } from '@nestjs/config';
